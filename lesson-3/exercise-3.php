f9202d4bb64b2f83c32a057e7493826fbe77a225
bellaAbyss <bella21abyss@gmail.com>
2016-02-12 00:05:06 +0300

    Third lesson.

--- lesson-3/exercise-3.php
+++ lesson-3/exercise-3.php
@@ -35,5 +35,21 @@ echo "<p style=\"font-weight: 700;\">Задание 3.</p>";
 
 echo "<p style=\"font-weight: 700;\">Задание 4.</p>";
 
-
+$arr  = array(
+    "Московская область" => array("Клин", "Лобня", "Химки"),
+    "Ленинградская область" => array("Пушкино", "Петергоф", "Кронштадт"),
+    "Калужская область" => array("Обнинск", "Медынь", "Козельск")
+);
+echo "<pre>";
+foreach ($arr as $item => $region) {
+    echo "<p>$item:</p>";
+    $f = true;
+    foreach ($region as $city) {
+        if (!$f)
+            echo ", $city";
+        else echo "\t$city";
+        $f = false;
+    }
+}
+echo "</pre>";
 ?>
