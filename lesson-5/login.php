213d01d87a924eb67b00d633be64aafe4fca8837
Darya <bella21abyss@gmail.com>
2016-02-18 18:06:47 +0300

    Fifth lesson was added.

---
+++ lesson-5/login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davakir
+ * Date: 18.02.16
+ * Time: 17:56
+ */
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+</head>
+<body>
+
+</body>
+</html>
