042569f3a0c4a9c1bcdec902d17f935730b153ee
bellaAbyss <bella21abyss@gmail.com>
2016-02-27 20:05:44 +0000

    Delete comment

--- lesson-7/functions/mysql_db_service.php
+++ lesson-7/functions/mysql_db_service.php
@@ -1,17 +1,6 @@
 <?php
 header("Content-type: text/html; charset=UTF-8");
 
-// function connect($db_name = "test") {
-//     $host = "localhost:3306";
-//     $user = "root";
-//     $password = "p1010942342";
-//     $connection = mysqli_connect($host, $user, $password, $db_name);
-//     if (!$connection) {
-//         die('Не удалось подключиться к базе:' . mysqli_connect_error());
-//     }
-//     return $connection;
-// }    
-
 function connect($db_name = "test") {
     $mysql_host = "localhost:3306";
     $mysql_login = "root";
