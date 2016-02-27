e297697e4f2fccd2023981b5cfd250c3c5f3b005
bellaAbyss <bella21abyss@gmail.com>
2016-02-27 11:35:35 +0000

    Show image 70% size of the <body>

--- lesson-7/photo.php
+++ lesson-7/photo.php
@@ -1,3 +1,9 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Photo</title>
+</head>
+<body style="text-align:center;">
+    <img src="<?php echo $_GET['img_name']; ?>" width="70%" />
+</body>
+</html>
