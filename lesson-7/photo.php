ed18564dee3745d89a3d84c0d7fc3fee7dcec13b
bellaAbyss <bella21abyss@gmail.com>
2016-02-27 20:10:22 +0000

    Get file name from gallery.php, show reviews' counter under the image.

--- lesson-7/photo.php
+++ lesson-7/photo.php
@@ -1,9 +1,20 @@
+<?php
+    require_once('functions/reviews.php');
+    $name = explode("/", $_GET['img_name']);
+    $name = $name[1];
+?>
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8"/>
     <title>Photo</title>
 </head>
 <body style="text-align:center;">
     <img src="<?php echo $_GET['img_name']; ?>" width="70%" />
+    <p>Количество просмотров: <?php echo get_reviews($name); ?></p>
 </body>
 </html>
+
+<?php
+    review_plus($name);
+?>
